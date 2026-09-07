6ef5f0d3272e533aa02dfa455a0c786d4b7865a4
g.petraroli <g.petraroli@gmail.com>
2026-09-07 11:51:19 +0200

    Enhance Docker entrypoint scripts for cache and log management
    - Added directory creation for cache, log, and share paths in both entrypoint.dev.sh and entrypoint.prod.sh.
    - Included ownership change for the /var/www/app/var directory to ensure proper permissions for the www-data user.

--- docker/entrypoint.dev.sh
+++ docker/entrypoint.dev.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+mkdir -p /var/www/app/var/cache /var/www/app/var/log /var/www/app/var/share
+
 php ./bin/console d:m:m --no-interaction
+
+chown -R www-data:www-data /var/www/app/var
 
 php-fpm
--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+mkdir -p /var/www/app/var/cache /var/www/app/var/log /var/www/app/var/share
+
 composer dump-env prod
 
 php ./bin/console d:m:m --no-interaction
+
+chown -R www-data:www-data /var/www/app/var
 
 php-fpm
